1d007ea0b48d806f4374eb1d57a211b550e8bd9e
JP <dawrkshado@gmail.com>
2025-06-24 22:42:23 +0800

    e

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const gap = 16;
-const carousel = document.getElementsByClassName("TRPics");
-const buttonLeft = document.getElementById("");
+const carousel = document.getElementsByClassName("TRPics"),
+buttonLeft = document.getElementById(""),
+next = document.getElementById("");
