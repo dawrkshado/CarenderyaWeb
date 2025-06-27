64562d3e16701b1644acc75b733b69ecddb08deb
JP <dawrkshado@gmail.com>
2025-06-27 22:15:09 +0800

    e

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-const TRPics = document.getElementsByClassName("TRPics");
-const buttonLeft = document.getElementById("buttonLeft");
+const TRPics = document.querySelector(".TRPics");
+const  Prev= document.getElementById("buttonLeft");
+const  Next= document.getElementById("buttonRight");
+
+//if scrolled, the image carousels
+TRPics.addEventListener("wheel", (e) => {
+    e.preventDefault();
+    TRPics.scrollLeft += e.deltaY
+});
+
+Next.addEventListener("click",() =>
+{
+    TRPics.scrollLeft += 1150;
+})
+
+Prev.addEventListener("click", () =>{
+    TRPics.scrollLeft -= 1150;
+})
